a55c51e3c2a2397fd80b60625aef8de750aaf301
MustafaGungor <mustafagungormg@outlook.com>
2018-04-11 15:40:13 +0300

    #change the RadioInput props

--- showcase/components/inputs/RadioInputExample.tsx
+++ showcase/components/inputs/RadioInputExample.tsx
@@ -5,9 +5,37 @@ import RadioInput from "../../../src/inputs/RadioInput";
 export default class RadioInputExample extends React.Component<any,any>{
     constructor(props:any){
         super(props);
+        this.state = {
+            radioInputInline : "4",
+            radioInputHori : "1"
+        }
     }
     render(){
-        return <RadioInput onClick={this.handleChange.bind(this)}/>
+        let value= [
+            {id:1,value:"XxxX",des:"D1"},
+            {id:2,value:"XyyX",des:"D1"},
+            {id:3,value:"XzzX",des:"D1"},
+            {id:4,value:"XaaX",des:"D1"},
+        ];
+        return <div>
+            <label>Inline RadioInput Example</label>
+            <RadioInput
+                  name={"radioInputInline"}
+                  value={this.state.radioInputInline}
+                  inline
+                  items={value}
+                  idField="id"
+                  textField="value"
+                  onChange={this.handleChange.bind(this)}/>
+            <label>Horizontal RadioInput Example</label>
+            <RadioInput
+                name={"radioInputHori"}
+                value={this.state.radioInputHori}
+                items={value}
+                idField="id"
+                textField="value"
+                onChange={this.handleChange.bind(this)}/>
+        </div>
     }
     handleChange(e){
         let name = e.target.name;
--- src/inputs/RadioInput.tsx
+++ src/inputs/RadioInput.tsx
@@ -1,32 +1,55 @@
 import * as React from "react";
 import "./input.css"
+import {checked} from "glamor";
 
-export default class RadioInput extends React.Component<any,any>{
+export interface RadioInputProps{
+    inline?:boolean;
+    value?:string;
+    idField?:string | any;
+    textField?:string | any;
+    onChange?:any;
+    items?: Array<any>;
+    name?:string;
+    checked?:boolean;
+}
+
+export default class RadioInput extends React.Component<any,RadioInputProps>{
     constructor(props:any){
         super(props);
     }
 
     render(){
-        return <form><div onClick={this.onChange.bind(this)} className={"form-control"}>{this.returnRadioElements(this)}</div></form>
+        return <form><div onChange={this.onChange.bind(this)} className={"form-control"}>{this.returnRadioElements(this,this.props.items)}</div></form>
     }
 
-    private returnRadioElements(e:any):any{
-        let value= [
-            {id:1,value:"XxxX"},
-            {id:2,value:"XyyX"},
-            {id:3,value:"XzzX"},
-            {id:4,value:"XaaX"},
-            ];
-        let component:Array<any> = [];
-        value.forEach(function (v) {
-            component.push(<div><input
-                className={"radio"}
-                type="radio"
-                key={v.id}
-                name="base"
-                value={v.value} /><label>{v.value}</label></div>);
-        });
+    private returnRadioElements(e:any,value:Array<any>):any{
 
+        let component:Array<any> = [];
+        let inline = false;
+        if(this.props.inline || this.props.inline == true){
+            inline = true;
+        }
+        let checkValue = e.props.value;
+        value.forEach(function (v:any) {
+            let isChecked = v.id == checkValue ? true: false ;
+            if(inline == true){
+                component.push(<label style={{paddingLeft: "2px"}}><label><input
+                    className={"radio"}
+                    type="radio"
+                    checked={isChecked}
+                    key={v.id}
+                    name={e.props.name}
+                    value={v.id} />{v.value}</label></label>)
+            }else{
+                component.push(<div><label><input
+                    className={"radio"}
+                    type="radio"
+                    checked={isChecked}
+                    key={v.id}
+                    name={e.props.name}
+                    value={v.id} />{v.value}</label></div>)
+            }
+        }.bind(e));
         return component;
     }
 
