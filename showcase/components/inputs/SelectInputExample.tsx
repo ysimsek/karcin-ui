fdf1b6da7252972ef9d7c2439a5e1afabf344559
Deniz Dalkılıç <ddalkilic2@gmail.com>
2018-05-07 11:52:56 +0300

    select input bugfix

--- showcase/components/inputs/SelectInputExample.tsx
+++ showcase/components/inputs/SelectInputExample.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
-import {SelectInput} from "karcin-ui";
+import {SelectInput, Panel} from "karcin-ui";
 
 
 const items = [
     {
-        id:1,name:"Mustafa",userName:"MustafaGungorMG",project:"KARÇİN-Uİ"
+        id:1,name:"Mustafa",userName:"MustafaGungorMG",project:"KARÇİN-Uİ", img:"https://img.wikinut.com/img/anxidmiwlv2yy1ar/jpeg/36x36/Author-profile-image.jpeg"
     },
     {
-        id:2,name:"Mustafa",userName:"MustafaGungorMG",project:"KARÇİN-BACKEND"
+        id:2,name:"Mustafa",userName:"MustafaGungorMG",project:"KARÇİN-BACKEND", img: "https://d3ui957tjb5bqd.cloudfront.net/images/users/18/185/185868/avatar-50-50.jpg?1524845263"
     },
     {
-        id:3,name:"Mustafa",userName:"MustafaGungorMG",project:"KARÇİN-SKELETON"
+        id:3,name:"Mustafa",userName:"MustafaGungorMG",project:"KARÇİN-SKELETON", img: "http://worldonline.media.clients.ellingtoncms.com/img/profiles/2010/Feb/19/lee-head-avatar-small_r48x48.jpg?5dda7ebe3a0a47b731bc018fa5259827222aab62"
     },
     {
-        id:4,name:"Mustafa",userName:"MustafaGungorMG",project:"APACHE KARAF"
-    },
+        id:4,name:"Mustafa",userName:"MustafaGungorMG",project:"APACHE KARAF", img: "https://img.wikinut.com/img/anxidmiwlv2yy1ar/jpeg/36x36/Author-profile-image.jpeg"
+    }
 
 ];
 
@@ -27,21 +27,65 @@ export default class SelectInputExample extends React.Component<any,any>{
     }
     render(){
         return <div>
-            <SelectInput
-                name={"selectInput"}
-                label={"SelectInput Example"}
-                items={items}
-                id={"id"}
-                value={"project"}
-                item={this.state.selectInput}
-                onChange={this.handleChange.bind(this)}
-            />
+            <Panel title="Single Select" collapse={true} collapsible={true}>
+                <SelectInput
+                    name={"selectInput"}
+                    label={"SelectInput Example"}
+                    items={items}
+                    id={"id"}
+                    value={"project"}
+                    onChange={this.handleChange.bind(this)}
+                />
+            </Panel>
+            <Panel title="Multi Select" collapse={true} collapsible={true}>
+                <SelectInput
+                    name={"selectInput"}
+                    label={"SelectInput Example"}
+                    items={items}
+                    id={"id"}
+                    value={"project"}
+                    onChange={this.handleChange.bind(this)}
+                    type={"multi"}
+                />
+            </Panel>
+            <Panel title="Multi Select Renderer" collapse={true} collapsible={true}>
+                <SelectInput
+                    name={"selectInput"}
+                    label={"SelectInput Example"}
+                    items={items}
+                    id={"id"}
+                    value={"project"}
+                    onChange={this.handleChange.bind(this)}
+                    type={"multi"}
+                    renderer={this.rendererDropDown}
+                />
+            </Panel>
+
+            <Panel title="Multi Select Renderer & Selected Renderer" collapse={true} collapsible={true}>
+                <SelectInput
+                    name={"selectInput"}
+                    label={"SelectInput Example"}
+                    items={items}
+                    id={"id"}
+                    value={"project"}
+                    onChange={this.handleChange.bind(this)}
+                    type={"multi"}
+                    renderer={this.rendererDropDown}
+                    selectedRenderer={this.selectRenderer}
+                />
+            </Panel>
+            
         </div>
     }
     handleChange(e){
-        let name = e.target.name;
-        let state = [];
-        state[e.target.name] = e.target.parsedValue != undefined ? e.target.parsedValue : e.target.value;
-        this.setState(state);
+        
+    }
+
+    rendererDropDown(value:any){
+        return <div><div className="img"><img src={value["img"]}/></div><span>{value["project"]}</span></div>;
+    }
+
+    selectRenderer(value:any){
+        return <div><div className="img"><img src={value["img"]}/></div><span>{value["project"]}</span></div>;
     }
 }
