dcb50e929cdfe427b2a77baea7c62234a86e0a9b
Deniz Dalkılıç <ddalkilic2@gmail.com>
2018-05-17 17:27:25 +0300

    button example edit

--- showcase/components/examples/ButtonExample.tsx
+++ showcase/components/examples/ButtonExample.tsx
@@ -1,30 +1,21 @@
 import * as React from "react";
 import {Button} from 'karcin-ui';
-import Store from '../../../store/Store';
 
 export default class ButtonExample extends React.Component<any, any> {
-    store = null;
-    constructor(props:any){
-        super(props);
 
-        this.store = new Store({
-            idField : 'id',
-            data: [{ id: 1, name: "John", surname: "Doe" }, { id: 2, name: "Jane", surname: "Roe" }]
-        }, this.onSuccess);
-
-       // this.store.create({id :5, name: "Deneme", surname: "asd"}, (vl)=>{this.onSuccess(vl)});
-    }
     render() {
         return (
             <div>
-                <Button color="primary" onClick={()=>{this.onError()}}>primary</Button>{' '}
-                <Button color="secondary" onClick={()=>{this.onError2()}}>secondary</Button>{' '}
+                <Button color="primary" onClick={() => {
+                    this.onClick()
+                }}>primary</Button>{' '}
+                <Button color="secondary">secondary</Button>{' '}
                 <Button color="success">success</Button>{' '}
                 <Button color="info">info</Button>{' '}
                 <Button color="warning">warning</Button>{' '}
                 <Button color="danger">danger</Button>{' '}
-                <Button color="dark" onClick={()=>{this.onError()}}>dark</Button>{' '}
-                <Button color="light" onClick={()=>{this.onError()}}>light</Button>{' '}
+                <Button color="dark">dark</Button>{' '}
+                <Button color="light">light</Button>{' '}
                 <Button color="link">link</Button>
                 <hr/>
                 <Button outline color="primary">primary</Button>{' '}
@@ -48,16 +39,6 @@ export default class ButtonExample extends React.Component<any, any> {
         );
     }
 
-    onSuccess(res:any) {
-        console.log(res);
-    }
-
-    onError() {
-        console.log(this.store.reset());
-    }
-
-    onError2(){
-        let data = [{ id: 1, name: "Deniz", surname: "Dalkılıç" }, { id: 2, name: "name", surname: "surname" }];
-        console.log(this.store.update(data));
+    onClick() {
     }
 }
