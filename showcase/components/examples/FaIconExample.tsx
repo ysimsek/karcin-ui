f110390b5ec7439035cfbfba94072f371df0effe
Administrator <mustafa.gungor@asiselektronik.com.tr>
2018-10-04 14:04:43 +0300

    v0.0.70 Faicon zenginleştirildi ve color özelliği eklendi.

--- showcase/components/examples/FaIconExample.tsx
+++ showcase/components/examples/FaIconExample.tsx
@@ -5,20 +5,32 @@ export default class FaIconExample extends React.Component<any, any> {
     render() {
         return (
             <div>
+
+                <hr/>
                 <FaIcon code="fa-html5"></FaIcon>
                 <FaIcon code="fa-html5" size="fa-2x"></FaIcon>
                 <FaIcon code="fa-html5" size="fa-3x"></FaIcon>
                 <FaIcon code="fa-html5" size="fa-4x"></FaIcon>
                 <FaIcon code="fa-html5" size="fa-5x"></FaIcon>
+                <FaIcon code="fa-html5" size="fa-6x"></FaIcon>
+                <FaIcon code="fa-html5" size="fa-7x"></FaIcon>
+                <FaIcon code="fa-html5" size="fa-8x"></FaIcon>
+                <FaIcon code="fa-html5" size="fa-9x"></FaIcon>
                 <hr />
-                <FaIcon code="fa-facebook-square" size="fa-2x"></FaIcon>
-                <FaIcon code="fa-twitter-square" size="fa-2x"></FaIcon>
-                <FaIcon code="fa-youtube-square" size="fa-2x"></FaIcon>
-                <FaIcon code="fa-google-plus-square" size="fa-2x"></FaIcon>
-                <FaIcon code="fa-instagram" size="fa-2x"></FaIcon>
+                <FaIcon color={"primary"} code="fas fa-500px" size="fa-3x"></FaIcon>
+                <FaIcon color={"secondary"} code="fa-facebook-square" size="fa-3x"></FaIcon>
+                <FaIcon color={"success"} code="fa-twitter-square" size="fa-3x"></FaIcon>
+                <FaIcon color={"info"} code="fab fa-amazon" size="fa-3x"></FaIcon>
+                <FaIcon color={"warning"} code="fa-google-plus-square" size="fa-3x"></FaIcon>
+                <FaIcon color={"danger"} code="fa-instagram" size="fa-3x"></FaIcon>
+                <FaIcon color={"dark"} code="fas fa-american-sign-language-interpreting" size="fa-3x"></FaIcon>
+                <FaIcon color={"light"} code="fas fa-angle-double-left" size="fa-3x"></FaIcon>
+                <FaIcon code="fas fa-angle-double-right" size="fa-3x"></FaIcon>
+                <FaIcon code="fab fa-angellist" size="fa-3x"></FaIcon>
                 <hr />
-                <FaIcon code="fa-spinner fa-spin" size="fa-2x"></FaIcon>
-                <FaIcon code="fa-cog fa-spin" size="fa-2x"></FaIcon>
+
+                <FaIcon color={"primary"} code="fa-spinner fa-spin" size="fa-6x"></FaIcon>
+                <FaIcon color={"success"} code="fa-cog fa-spin" size="fa-6x"></FaIcon>
             </div>
         );
     }
