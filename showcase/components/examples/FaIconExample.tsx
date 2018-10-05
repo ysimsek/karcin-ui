fa9fe3f9ef816ecfc28f0878bf1a100a1d100b3f
Administrator <mustafa.gungor@asiselektronik.com.tr>
2018-10-05 21:06:07 +0300

    FaIcon example geliştirildi.

--- showcase/components/examples/FaIconExample.tsx
+++ showcase/components/examples/FaIconExample.tsx
@@ -1,37 +1,63 @@
 import * as React from "react";
-import {FaIcon} from 'karcin-ui';
+import {FaIcon, Badge} from 'karcin-ui';
+const iconList = require("./../models/FaIconModel.json");
 
 export default class FaIconExample extends React.Component<any, any> {
-    render() {
-        return (
-            <div>
 
-                <hr/>
-                <FaIcon code="fa-html5"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-2x"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-3x"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-4x"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-5x"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-6x"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-7x"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-8x"></FaIcon>
-                <FaIcon code="fa-html5" size="fa-9x"></FaIcon>
-                <hr />
-                <FaIcon color={"primary"} code="fas fa-500px" size="fa-3x"></FaIcon>
-                <FaIcon color={"secondary"} code="fa-facebook-square" size="fa-3x"></FaIcon>
-                <FaIcon color={"success"} code="fa-twitter-square" size="fa-3x"></FaIcon>
-                <FaIcon color={"info"} code="fab fa-amazon" size="fa-3x"></FaIcon>
-                <FaIcon color={"warning"} code="fa-google-plus-square" size="fa-3x"></FaIcon>
-                <FaIcon color={"danger"} code="fa-instagram" size="fa-3x"></FaIcon>
-                <FaIcon color={"dark"} code="fas fa-american-sign-language-interpreting" size="fa-3x"></FaIcon>
-                <FaIcon color={"light"} code="fas fa-angle-double-left" size="fa-3x"></FaIcon>
-                <FaIcon code="fas fa-angle-double-right" size="fa-3x"></FaIcon>
-                <FaIcon code="fab fa-angellist" size="fa-3x"></FaIcon>
-                <hr />
+        constructor(props:any){
+                super(props);
+                this.state = {
+                    icon: "fa-html5"
+                }
 
-                <FaIcon color={"primary"} code="fa-spinner fa-spin" size="fa-6x"></FaIcon>
-                <FaIcon color={"success"} code="fa-cog fa-spin" size="fa-6x"></FaIcon>
-            </div>
-        );
-    }
+        }
+        render() {
+                return <div>
+                    {this.getFaIconList(iconList)}
+                    {this.getFaIcon()}
+                    </div>
+        }
+
+        getFaIconList(iconList:any){
+                let component = [];
+                iconList.fields.map((icon,idx)=>{
+                    component.push( <FaIcon code={icon} key={icon} onClick={this.onClick.bind(this)}></FaIcon>)
+                })
+                return component
+        }
+
+        getFaIcon(){
+                return <div>
+                            <hr/>
+                            <Badge color={"primary"}>{"<FaIcon code='"+this.state.icon+"'/>"}</Badge>
+                            <hr/>
+                            <FaIcon code={this.state.icon}></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-2x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-4x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-5x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-6x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-7x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-8x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-9x"></FaIcon>
+                            <hr />
+                            <FaIcon color={"primary"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon color={"secondary"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon color={"success"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon color={"info"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon color={"warning"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon color={"danger"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon color={"dark"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon color={"light"} code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-3x"></FaIcon>
+                            <FaIcon code={this.state.icon} size="fa-3x"></FaIcon>
+                            <hr />
+                            <FaIcon color={"primary"} code={this.state.icon+" fa-spin"} size="fa-6x"></FaIcon>
+                            <FaIcon color={"success"} code={this.state.icon+" fa-spin"} size="fa-6x"></FaIcon>
+                </div>
+            }
+
+            onClick(e){
+               this.setState({icon : e.target.classList[2]});
+            }
 }
