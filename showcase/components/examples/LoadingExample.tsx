81ca748de5f5939c329f3bf14b76dd89e2d8cbfe
Administrator <mustafa.gungor@asiselektronik.com.tr>
2018-11-15 11:07:25 +0300

    Change the loading component and example.

--- showcase/components/examples/LoadingExample.tsx
+++ showcase/components/examples/LoadingExample.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Loading, Button} from 'karcin-ui';
+import {Loading, Button , Panel} from 'karcin-ui';
 import {Row,Col} from "reactstrap";
 
 export default class LoadingExample extends React.Component<any, any> {
@@ -14,24 +14,56 @@ export default class LoadingExample extends React.Component<any, any> {
     render() {
 
         return (<div>
-                <Row id={"dd"}>
-                    dsadsads<br/>
-                    sdada<br/>
+                <span className="example-reagent">Row example inset loading</span>
+                <Row id={"inRow"} style={{border:"2px solid gray",padding:5}}>
+                    <p>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.</p>
+                    <p>Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak
+                        karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.</p>
+                    <p>Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır.</p>
+                    <p>1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.</p>
                 </Row>
-                <Button color="primary" onClick={this.loadingOpen.bind(this)}>Loading Open</Button>
+                <p>
+                    <span className="example-reagent">Loading Inset</span>
+                    <Button onClick={this.loadingOpen.bind(this)}>default</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"primary"}>primary</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"success"}>success</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"warning"}>warning</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"danger"}>danger</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"info"}>info</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"secondary"}>secondary</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"dark"}>dark</Button>{' '}
+                    <Button onClick={this.loadingOpen.bind(this)} color={"light"}>light</Button>{' '}
+
+
+                    <span className="example-reagent">Loading Full</span>
+                    <Button onClick={this.loadingOpenBody.bind(this)}>default</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"primary"}>primary</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"success"}>success</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"warning"}>warning</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"danger"}>danger</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"info"}>info</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"secondary"}>secondary</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"dark"}>dark</Button>{' '}
+                    <Button onClick={this.loadingOpenBody.bind(this)} color={"light"}>light</Button>{' '}
+                </p>
             </div>
         );
     }
 
-    loadingClose() {
+    loadingClose(id?:string | number) {
         let self = this;
         setTimeout(() => {
-                Loading.remove();
+                id != undefined ? Loading.remove({id:id}) : Loading.remove();
         }, 3000);
     }
 
-    loadingOpen () {
-        Loading.add({id:"dd",label:"Loading..."});
-		// this.loadingClose();
+    loadingOpen (e) {
+        Loading.add({id:"inRow",label:"Loading...",color:e.target.textContent != "default" ? e.target.textContent : undefined});
+		this.loadingClose("inRow");
+    }
+
+    loadingOpenBody(e){
+        Loading.add({color:e.target.textContent != "default" ? e.target.textContent : undefined});
+        this.loadingClose();
     }
 }
--- src/functional/loading/Loading.tsx
+++ src/functional/loading/Loading.tsx
@@ -4,29 +4,26 @@ class Loading{
     loading:any;
 
     add(obj?:any){
-
-
-
         this.element = document.createElement('div');
         this.element.classList.add("loading");
         var name = obj != undefined ? (obj.label != undefined ? obj.label : "" ): "";
         var id:any = obj != undefined ? (obj.id != undefined ? document.getElementById(obj.id) : null) : null;
-
+        var color = obj != undefined ? (obj.color != undefined ? " "+obj.color:"") : "";
         if(id != null) {
             this.element.innerHTML =
                 '<div class="pre-loading show">\n' +
-                '    <span class="glyphicon glyphicon-inset glyphicon-refresh spinning"></span>\n' +
-                '<div class="pre-loading-text">' + name + '</div>'
+                '    <span class="glyphicon glyphicon-inset glyphicon-refresh spinning' +
+                color +
+                '"></span>\n' +
             '</div>';
-
+            this.element.setAttribute("id","setLoadIdOfSystem")
             id.appendChild(this.element);
             id.style.setProperty("position", "relative");
             document.body.style.setProperty("overflow", "hidden");
         }else{
             this.element.innerHTML =
                 '<div class="pre-loading show">\n' +
-                '    <span class="glyphicon glyphicon-refresh spinning"></span>\n' +
-                '<div class="pre-loading-text">' + name + '</div>'
+                '    <span class="glyphicon glyphicon-refresh spinning'+ color+'"></span>\n'
             '</div>';
             document.body.style.setProperty("overflow", "hidden");
             document.body.appendChild(this.element);
@@ -34,8 +31,14 @@ class Loading{
 
     }
 
-    remove(obj?:string){
-        document.body.removeChild(this.element)
+    remove(obj?:any){
+        try {
+            obj != undefined ?(
+            obj.id != undefined ? document.getElementById(obj.id).removeChild(document.getElementById("setLoadIdOfSystem")) :
+                document.body.removeChild(this.element)) : document.body.removeChild(this.element)
+        }catch (e){
+            console.log(e)
+        }
     }
 
 }
