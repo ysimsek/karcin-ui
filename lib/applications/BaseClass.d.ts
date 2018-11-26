5ad68643696ae577ba3f56d2594209f1a927c588
Deniz Dalkılıç <deniz.dalkilic@asiselektronik.com.tr>
2018-11-26 13:43:15 +0300

    merge

--- lib/applications/BaseClass.d.ts
+++
@@ -1,7 +0,0 @@
-export default class BaseClass {
-    constructor(props: any);
-    bindAll(instance: any): void;
-    response(callback?: any, callbackData?: any, count?: any): any;
-    static mappingDataFind(response: any, mapping: any): any;
-    static findResponseData(response: any, mapping: any): any;
-}
